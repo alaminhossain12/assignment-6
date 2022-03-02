a4247fce2255245f89a5cf8d3da7c4d6619a2fbe
alamin hossain <mh1134938@gmail.com>
2022-03-02 17:58:41 +0600

    function parts

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 // display details of selected phone 
 const detailContainer = document.getElementById('display-detail');
+
+// Function for Spinner 
+const spinner = (displayStyle) => {
+  document.getElementById("spinner-border").style.display = displayStyle;
+};
+spinner('none');
+
+// Search Phone Name 
+const searchPhone = () => {
+  spinner('block');
+  const searchValue = document.getElementById("search-field").value;
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayPhones(data.data));
+  //   Clear input field, detail container
+  document.getElementById("search-field").value = "";
+  detailContainer.innerHTML = '';
+  detailContainer.classList.remove('display-detail');
+  heading('detail-heading', 'none');
+}
