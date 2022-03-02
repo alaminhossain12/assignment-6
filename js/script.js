e907065c3081fe8205b113b96bee0f1f3bde9bd4
alamin hossain <mh1134938@gmail.com>
2022-03-02 18:03:11 +0600

    added all function

--- js/script.js
+++ js/script.js
@@ -59,3 +59,39 @@ const displayPhones = (phones) => {
   heading('display-error', 'none');
   }
 };
+
+// Display Explore button Parts
+const displayPhoneDetails = (info) => {
+  const sensorsInfo = info.mainFeatures.sensors;
+  // Class added for adding style 
+  detailContainer.classList.add('display-detail');
+  const div = document.createElement('div');
+  div.classList.add('col');
+  div.innerHTML = `
+      <div class="card">
+          <img src="${info.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+              <h4 class="card-title">${info.name}</h4>
+              <h5 class="card-title"><strong>Brand: ${info.brand}</strong></h5>
+              <p class="card-text">${ info.releaseDate ? info.releaseDate : 'Release date not found!'} </p>
+              <h5 class="card-title"><strong>Main Features:</strong></h5>
+              <hr>
+              <p class="card-text"><strong>Chipset:</strong> ${info.mainFeatures.chipSet}</p>
+              <p class="card-text"><strong>Memory:</strong> ${info.mainFeatures.memory}</p>
+              <p class="card-text"><strong>Storage:</strong> ${info.mainFeatures.storage}</p>
+              <p class="card-text"><strong>Display Size:</strong> ${info.mainFeatures.displaySize}</p>  
+              <h5 class="card-title"><strong>Sensors Information:</strong></h5>   
+              <hr>            
+              <p class="card-text"><strong> ${sensorsInfo.join(', ')}</strong></p>  
+              <h5 class="card-title"><strong>Others Information:</strong></h5> 
+              <hr>              
+              <p class="card-text"><strong>WLAN: </strong> ${ info.others?.WLAN ? info.others?.WLAN : 'WLAN not found!'} </p>
+              <p class="card-text"><strong>Bluetooth: </strong> ${ info.others?.Bluetooth ? info.others?.Bluetooth : 'Bluetooth not found!'} </p>
+              <p class="card-text"><strong>GPS: </strong> ${ info.others?.GPS ? info.others?.GPS : 'GPS not found!'} </p>
+              <p class="card-text"><strong>USB: </strong> ${ info.others?.USB ? info.others?.USB : 'USB not found!'} </p>
+              <p class="card-text"><strong>Radio: </strong> ${ info.others?.Radio ? info.others?.Radio : 'Radio not found!'} </p>
+          </div>
+      </div> 
+  `;
+  detailContainer.appendChild(div);       
+};
