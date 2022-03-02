f245eb93fbbedcb8298ccbebc758819b5f7d9cd1
alamin hossain <mh1134938@gmail.com>
2022-03-02 18:01:18 +0600

    js error files

--- js/script.js
+++ js/script.js
@@ -21,3 +21,41 @@ const searchPhone = () => {
   detailContainer.classList.remove('display-detail');
   heading('detail-heading', 'none');
 }
+
+const displayPhones = (phones) => {
+  const phonesContainer = document.getElementById("display-search"); 
+  // Limit display upto 20
+  const phoneQunatity = phones.slice(0, 20);
+  if(phoneQunatity <= 0){
+      heading('display-error', 'block');
+      heading('search-heading', 'none');
+      heading('detail-heading', 'none');
+      detailContainer.innerHTML = '';
+      detailContainer.classList.remove('display-detail');
+      phonesContainer.textContent = ''; 
+      phonesContainer.classList.remove('display-search') ;
+  }else {        
+      phonesContainer.classList.add('display-search') ;
+      // Display serach result    
+      phonesContainer.textContent = '';   
+  for (const phone of phoneQunatity) {
+      const div = document.createElement('div')
+      div.classList.add('col')
+      div.innerHTML = `
+          <div class="card">
+              <img src="${phone.image}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h4 class="card-title">${phone.phone_name}</h4>
+                  <h5 class="card-title">${phone.brand}</h5>
+                  <button onclick="loadPhoneDetails('${phone.slug}')">Explore</button>
+              </div>
+          </div> 
+      `;
+      phonesContainer.appendChild(div);
+    }
+    spinner('none');
+    // Display Section errors
+  heading('search-heading', 'block');
+  heading('display-error', 'none');
+  }
+};
